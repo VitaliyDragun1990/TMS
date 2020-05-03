1dd783f9d772a89eafa36bafe72328a1d8025bfe
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-03 12:10:09 +0300

    Show all teachers page

---
+++ timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/TeacherController.java
@@ -0,0 +1,28 @@
+package org.vdragun.tms.ui.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.vdragun.tms.core.application.service.TeacherService;
+
+/**
+ * Processes teacher-related requests
+ * 
+ * @author Vitaliy Dragun
+ *
+ */
+@Controller
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @GetMapping
+    public String showAllStudents(Model model) {
+        model.addAttribute("teachers", teacherService.findAllTeachers());
+        return "teachers";
+    }
+}
