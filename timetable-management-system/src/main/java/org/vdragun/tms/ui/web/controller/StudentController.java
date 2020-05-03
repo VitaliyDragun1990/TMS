76af051c3ecc2ce67e0209abbe176d97a4ea0c43
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-03 16:13:35 +0300

    Simple student page

--- timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/StudentController.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.vdragun.tms.core.application.service.StudentService;
 
@@ -24,5 +25,11 @@ public class StudentController {
     public String showAllStudents(Model model) {
         model.addAttribute("students", studentService.findAllStudents());
         return "students";
+    }
+
+    @GetMapping("/{studentId}")
+    public String showStudentInfo(@PathVariable("studentId") Integer studentId, Model model) {
+        model.addAttribute("student", studentService.findStudentById(studentId));
+        return "student";
     }
 }
