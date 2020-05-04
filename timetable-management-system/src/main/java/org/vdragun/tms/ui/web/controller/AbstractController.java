5f2b9981342d9626334f47ec5c64da11965f1252
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-04 10:08:43 +0300

    AbstractController parent class

---
+++ timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/AbstractController.java
@@ -0,0 +1,45 @@
+package org.vdragun.tms.ui.web.controller;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+/**
+ * Parent class with common functionality for all application controllers.
+ * 
+ * @author Vitaliy Dragun
+ *
+ */
+abstract class AbstractController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MessageSource messageSource;
+
+    protected String getMessage(String code, Object... args) {
+        return messageSource.getMessage(code, args, getLocale());
+    }
+
+    protected String formatDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(getMessage("format.date"));
+        return date.format(formatter);
+    }
+
+    protected String formatMonth(Month month) {
+        return month.getDisplayName(TextStyle.FULL, Locale.US);
+    }
+
+    protected Locale getLocale() {
+        return LocaleContextHolder.getLocale();
+    }
+
+}
