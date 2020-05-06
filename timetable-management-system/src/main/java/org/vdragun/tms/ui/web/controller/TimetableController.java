43a10187d865d396fa9ad113bd0d27eee9b128db
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-06 21:00:54 +0300

    Register new timetable without validation

--- timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/TimetableController.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/ui/web/controller/TimetableController.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.vdragun.tms.core.application.service.ClassroomService;
+import org.vdragun.tms.core.application.service.CourseService;
 import org.vdragun.tms.core.application.service.StudentService;
 import org.vdragun.tms.core.application.service.TeacherService;
+import org.vdragun.tms.core.application.service.TimetableData;
 import org.vdragun.tms.core.application.service.TimetableService;
 import org.vdragun.tms.core.domain.Student;
 import org.vdragun.tms.core.domain.Teacher;
@@ -40,6 +45,12 @@ public class TimetableController extends AbstractController {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private ClassroomService classroomService;
+
     @GetMapping
     public String showAllTimetables(Model model) {
         log.trace("Received GET request to show all timetables, URI={}", getRequestUri());
@@ -56,7 +67,27 @@ public class TimetableController extends AbstractController {
         log.trace("Received GET request to show data for timetable with id={}, URI={}", timetableId, getRequestUri());
         model.addAttribute(Attribute.TIMETABLE, timetableService.findTimetableById(timetableId));
         
-        return Page.TIMETABLE;
+        return Page.TIMETABLE_INFO;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        log.trace("Received GET request to show timetable registration form, URI={}", getRequestUri());
+        model.addAttribute(Attribute.TIMETABLE, new TimetableData());
+        model.addAttribute(Attribute.COURSES, courseService.findAllCourses());
+        model.addAttribute(Attribute.TEACHERS, teacherService.findAllTeachers());
+        model.addAttribute(Attribute.CLASSROOMS, classroomService.findAllClassrooms());
+        
+        return Page.TIMETABLE_FORM;
+    }
+
+    @PostMapping
+    public String registerNewTimetable(@ModelAttribute TimetableData timetableData, Model model) {
+        log.trace("Received POST request to register new timetable, data={}, URI={}", timetableData, getRequestUri());
+        Timetable timetable = timetableService.registerNewTimetable(timetableData);
+        model.addAttribute(Attribute.TIMETABLE, timetable);
+
+        return Page.TIMETABLE_INFO;
     }
 
     @GetMapping("/teacher/{teacherId}/day")
