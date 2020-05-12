0fe57e1df6de28ae79fc35c88dc2003efd5785e1
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-12 11:09:28 +0300

    Simplify WebConfig.java by moving some configuration to property file

--- timetable-management-system/src/main/java/org/vdragun/tms/config/WebConfig.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/config/WebConfig.java
@@ -2,82 +2,30 @@ package org.vdragun.tms.config;
 
 import java.util.Locale;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
 import org.vdragun.tms.ui.web.converter.StringToLocalDateCustomFormatter;
 import org.vdragun.tms.ui.web.converter.StringToLocalDateTimeCustomFormatter;
 import org.vdragun.tms.ui.web.converter.StringToTitleConverter;
 import org.vdragun.tms.ui.web.converter.TitleToStringConverter;
 import org.vdragun.tms.ui.web.util.Constants.Page;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingRespectLayoutTitleStrategy;
-
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "org.vdragun.tms.ui.web")
-public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+public class WebConfig implements WebMvcConfigurer {
 
-    private ApplicationContext applicationContext;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("classpath:/templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(false);
-
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.addDialect(new LayoutDialect(new GroupingRespectLayoutTitleStrategy()));
-        templateEngine.addDialect(new Java8TimeDialect());
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        templateEngine.setTemplateEngineMessageSource(messageSource());
-
-        return templateEngine;
-    }
-
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(true);
-
-        return messageSource;
-    }
+    @Autowired
+    private MessageSource messageSource;
 
     @Bean
     public LocaleResolver localResolver() {
@@ -96,12 +44,12 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
     }
 
     @Bean
-    public StringToLocalDateCustomFormatter stringToLocalDateCustomFormatter(MessageSource messageSource) {
+    public StringToLocalDateCustomFormatter stringToLocalDateCustomFormatter() {
         return new StringToLocalDateCustomFormatter(messageSource);
     }
 
     @Bean
-    public StringToLocalDateTimeCustomFormatter stringToLocalDateTimeCustomFormatter(MessageSource messageSource) {
+    public StringToLocalDateTimeCustomFormatter stringToLocalDateTimeCustomFormatter() {
         return new StringToLocalDateTimeCustomFormatter(messageSource);
     }
 
@@ -111,28 +59,11 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
     }
 
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        resolver.setCharacterEncoding("UTF-8");
-        resolver.setOrder(1);
-        registry.viewResolver(resolver);
-    }
-
-    @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new TitleToStringConverter());
         registry.addConverter(new StringToTitleConverter());
-        registry.addFormatter(stringToLocalDateCustomFormatter(messageSource()));
-        registry.addFormatter(stringToLocalDateTimeCustomFormatter(messageSource()));
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/css/**", "/js/**", "/webjars/**")
-                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/", "/webjars/")
-                .resourceChain(false);
+        registry.addFormatter(stringToLocalDateCustomFormatter());
+        registry.addFormatter(stringToLocalDateTimeCustomFormatter());
     }
 
     @Override
