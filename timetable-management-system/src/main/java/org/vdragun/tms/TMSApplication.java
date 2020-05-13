f41c2af959ec17743277344706d7f7cff2173a39
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-13 14:55:38 +0300

    Update spring boot configuration: use explicit import

--- timetable-management-system/src/main/java/org/vdragun/tms/TMSApplication.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/TMSApplication.java
@@ -1,16 +1,22 @@
 package org.vdragun.tms;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.vdragun.tms.config.JPADaoConfig;
+import org.vdragun.tms.config.ServiceConfig;
+import org.vdragun.tms.config.StartupDataConfig;
+import org.vdragun.tms.config.WebConfig;
 
 /**
  * @author Vitaliy Dragun
  *
  */
-@SpringBootApplication(
-        scanBasePackages = "org.vdragun.tms.config",
-        exclude = { HibernateJpaAutoConfiguration.class })
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class })
+@Import({ JPADaoConfig.class, StartupDataConfig.class, ServiceConfig.class, WebConfig.class })
 public class TMSApplication {
 
     public static void main(String[] args) {
