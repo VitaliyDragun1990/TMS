eb0ecbcebcdef5621ecde81e21e232c3b13bad6c
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-05-20 21:19:50 +0300

    Add handler to GlobalErrorController for REST resources errors

--- timetable-management-system/src/main/java/org/vdragun/tms/ui/common/exception/GlobalErrorController.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/ui/common/exception/GlobalErrorController.java
@@ -1,21 +1,28 @@
 package org.vdragun.tms.ui.common.exception;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.vdragun.tms.ui.common.util.Constants.Attribute;
 import org.vdragun.tms.ui.common.util.Constants.Message;
 import org.vdragun.tms.ui.common.util.Constants.Page;
+import org.vdragun.tms.ui.common.util.Translator;
 
 /**
  * Application error controller
@@ -24,29 +31,37 @@ import org.vdragun.tms.ui.common.util.Constants.Page;
  *
  */
 @Controller
-public class GlobalErrorController implements ErrorController {
-
+@RequestMapping("/error")
+public class GlobalErrorController extends AbstractErrorController {
     private static final Logger log = LoggerFactory.getLogger(GlobalErrorController.class);
 
     @Autowired
-    private MessageSource messageSource;
+    private Translator translator;
 
-    @GetMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Object errorStatus = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (errorStatus != null) {
-            return showErrorPage(request, model, errorStatus);
-        } else {
-            // If no error - redirect to home page
-            return "redirect:/home";
-        }
+    public GlobalErrorController(ErrorAttributes errorAttributes, List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, errorViewResolvers);
     }
 
-    private String showErrorPage(HttpServletRequest request, Model model, Object errorStatus) {
-        Integer statusCode = Integer.valueOf(errorStatus.toString());
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public String showErrorPage(HttpServletRequest request, Model model) {
+        HttpStatus status = getStatus(request);
+        return showErrorPage(request, model, status);
+    }
 
-        if (statusCode == HttpStatus.NOT_FOUND.value()) {
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> provideResponse(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        log.warn("Handling '{}' request for URI:{} with status code:{}",
+                status.getReasonPhrase(), getRequestUrl(request), status.value());
+        if (status == HttpStatus.NO_CONTENT) {
+            return new ResponseEntity<>(status);
+        }
+        Map<String, Object> body = getErrorAttributes(request, false);
+        return new ResponseEntity<>(body, status);
+    }
+
+    private String showErrorPage(HttpServletRequest request, Model model, HttpStatus status) {
+        if (status == HttpStatus.NOT_FOUND) {
             log.warn("Handling not found resource, URI:{}", getRequestUrl(request));
             model.addAttribute(Attribute.MESSAGE, getMessage(Message.REQUESTED_RESOURCE, getRequestUrl(request)));
 
@@ -69,7 +84,7 @@ public class GlobalErrorController implements ErrorController {
     }
 
     private String getMessage(String code, Object... args) {
-        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+        return translator.getLocalizedMessage(code, args);
     }
 
 }
