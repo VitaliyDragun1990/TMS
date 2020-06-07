fa2a4a03b2a1b1252e83e92ffd200f731e7d2f46
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-06-07 15:38:52 +0300

    Define security roles: STUDENT, TEACHER, ADMIN

--- timetable-management-system/src/main/java/org/vdragun/tms/config/SecurityConfig.java
+++ timetable-management-system/src/main/java/org/vdragun/tms/config/SecurityConfig.java
@@ -1,26 +1,28 @@
 package org.vdragun.tms.config;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.vdragun.tms.security.rest.exception.AccessDeniedExceptionHandler;
 import org.vdragun.tms.security.rest.exception.RestAuthenticationEntryPoint;
-import org.vdragun.tms.security.rest.jwt.JwtConfigurer;
 import org.vdragun.tms.security.rest.jwt.JwtAuthenticationTokenFilter;
+import org.vdragun.tms.security.rest.jwt.JwtConfigurer;
 import org.vdragun.tms.security.rest.jwt.JwtTokenProvider;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -30,6 +32,7 @@ public class SecurityConfig {
 
     @Configuration
     @Order(1)
+    @Profile("!test")
     public static class RestWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         private static final String LOGIN_ENDPOINT = "/api/v1/auth/signin";
@@ -58,10 +61,23 @@ public class SecurityConfig {
                     .csrf().disable()
                     .authorizeRequests()
                     .antMatchers(REGISTER_ENDPOINT, LOGIN_ENDPOINT).permitAll()
-                    .antMatchers(HttpMethod.GET).permitAll()
-                    .antMatchers(HttpMethod.POST).hasAnyRole("USER", "ADMIN")
-                    .antMatchers(HttpMethod.PUT).hasAnyRole("USER", "ADMIN")
-                    .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                    // courses
+                    .antMatchers(GET, "/api/v1/courses", "/api/v1/courses/**").authenticated()
+                    .antMatchers(POST, "/api/v1/courses").hasRole("ADMIN")
+                    // students
+                    .antMatchers(GET, "/api/v1/students", "/api/v1/students/**").hasAnyRole("ADMIN", "TEACHER")
+                    .antMatchers(POST, "/api/v1/students").hasRole("ADMIN")
+                    .antMatchers(PUT, "/api/v1/students/**").hasRole("ADMIN")
+                    .antMatchers(DELETE, "/api/v1/students/**").hasRole("ADMIN")
+                    // teachers
+                    .antMatchers(GET, "/api/v1/teachers", "/api/v1/teachers/**").hasAnyRole("ADMIN", "TEACHER")
+                    .antMatchers(POST, "/api/v1/teachers").hasRole("ADMIN")
+                    // timetables
+                    .antMatchers(GET, "/api/v1/timetables", "/api/v1/timetables/**").authenticated()
+                    .antMatchers(POST, "/api/v1/timetables").hasRole("ADMIN")
+                    .antMatchers(PUT, "/api/v1/timetables/**").hasRole("ADMIN")
+                    .antMatchers(DELETE, "/api/v1/timetables/**").hasRole("ADMIN")
+                     // other
                     .anyRequest().authenticated()
                     .and().exceptionHandling()
                         .accessDeniedHandler(accessDeniedHandler)
@@ -72,6 +88,7 @@ public class SecurityConfig {
     }
 
     @Configuration
+    @Order(2)
     public static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         @Override
@@ -89,7 +106,7 @@ public class SecurityConfig {
         public void configure(WebSecurity web) throws Exception {
             web
                     .ignoring()
-                    .antMatchers(HttpMethod.GET, "/css/**", "/js/**", "/webjars/**");
+                    .antMatchers(GET, "/css/**", "/js/**", "/webjars/**");
         }
     }
 }
--- timetable-management-system/src/main/resources/sql/init_data.sql
+++ timetable-management-system/src/main/resources/sql/init_data.sql
@@ -1,8 +1,9 @@
-INSERT INTO roles (role_name) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
+INSERT INTO roles (role_name) VALUES ('ROLE_ADMIN'), ('ROLE_STUDENT'), ('ROLE_TEACHER');
 
 INSERT INTO USERS (username, first_name, last_name, email, password)
 VALUES 
-    ('admin', 'Admin', 'Superuser', 'admin@gmail.com', '$2y$04$bwzG1HjLiH5hVJtEaS3Wz.HNx4a6fbLeO0txIVpcNytdqaIFaeETO'),
-    ('user', 'User', 'Plainuser', 'user@gmail.com', '$2y$04$bwzG1HjLiH5hVJtEaS3Wz.HNx4a6fbLeO0txIVpcNytdqaIFaeETO');
+    ('admin', 'Admin', 'Administrator', 'admin@gmail.com', '$2y$04$bwzG1HjLiH5hVJtEaS3Wz.HNx4a6fbLeO0txIVpcNytdqaIFaeETO'),
+    ('student', 'Student', 'Student', 'student@gmail.com', '$2y$04$bwzG1HjLiH5hVJtEaS3Wz.HNx4a6fbLeO0txIVpcNytdqaIFaeETO'),
+    ('teacher', 'Teacher', 'Teacher', 'teacher@gmail.com', '$2y$04$bwzG1HjLiH5hVJtEaS3Wz.HNx4a6fbLeO0txIVpcNytdqaIFaeETO');
     
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1), (2, 2);
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1), (2, 2), (3, 3);
